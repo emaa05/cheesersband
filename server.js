d85729da602fd519669ae984ea7a39300bbb2e57
ema corra <emanuel.corradini@hotmail.com>
2023-12-14 00:34:58 -0300

    fix cors

--- server.js
+++ server.js
@@ -8,11 +8,7 @@ const path = require("path");
 
 const app = express();
 
-const corsOptions = {
-  origin: ["http://127.0.0.1:5500", "http://127.0.0.1:3000"],
-  credentials: true,
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 app.use(
